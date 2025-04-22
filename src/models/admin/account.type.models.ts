062b5287931d87b3334bbf3e4775ad580fe59929
Himanshu Chuchra <chuchrahimanshu@gmail.com>
2025-04-22 10:07:58 +0530

    Create AccountType Model Schema using AccountTypeSchemaInterface

---
+++ src/models/admin/account.type.models.ts
@@ -0,0 +1,32 @@
+// Import Section
+import mongoose from "mongoose";
+import type { Schema, Model } from "mongoose";
+
+// Schema Section
+const accountTypeSchema: Schema<AccountTypeSchemaInterface> =
+  new mongoose.Schema(
+    {
+      platform: {
+        type: String,
+        trim: true,
+        uppercase: true,
+        required: true,
+        unique: true,
+      },
+      isActive: {
+        type: Boolean,
+        required: true,
+        default: true,
+      },
+    },
+    {
+      timestamps: true,
+    }
+  );
+
+// Model Section
+const AccountType: Model<AccountTypeSchemaInterface> = mongoose.model(
+  "AccountType",
+  accountTypeSchema
+);
+export default AccountType;
