f6cf0bf9d2ce338b2057f45755470704ffb8c7e1
Himanshu Chuchra <chuchrahimanshu@gmail.com>
2025-04-22 10:35:26 +0530

    Create Reply Model Schema using ReplySchemainterface

---
+++ src/models/community/reply.models.ts
@@ -0,0 +1,64 @@
+// Import Section
+import mongoose from "mongoose";
+import type { Schema, Model } from "mongoose";
+
+// Schema Section
+const replySchema: Schema<ReplySchemaInterface> = new mongoose.Schema(
+  {
+    developer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Developer",
+      required: true,
+    },
+    comment: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+      required: true,
+    },
+    content: {
+      type: String,
+      trim: true,
+      maxlength: 500,
+      required: true,
+    },
+    attachments: [
+      {
+        url: String,
+        id: String,
+      },
+    ],
+    snippets: [
+      {
+        type: String,
+      },
+    ],
+    mentions: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Developer",
+      },
+    ],
+    pinned: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    hidden: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    isEdited: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Model Section
+const Reply: Model<ReplySchemaInterface> = mongoose.model("Reply", replySchema);
+export default Reply;
