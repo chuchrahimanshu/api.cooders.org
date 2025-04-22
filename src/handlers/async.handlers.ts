5286c3b545c88284becd8029717de679c7fe6f37
Himanshu Chuchra <chuchrahimanshu@gmail.com>
2025-04-22 12:06:20 +0530

    Create AsyncHandler utility function

---
+++ src/handlers/async.handlers.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from "express";
+
+function asyncHandler(
+  requestHandler: (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => Promise<void>
+) {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await requestHandler(req, res, next);
+    } catch (error) {
+      console.log(`ERROR: ${req.method} ${req.originalUrl}:\n`, error);
+      next(error);
+    }
+  };
+}
+
+export default asyncHandler;
---
+++ src/handlers/index.handlers.ts
@@ -0,0 +1,5 @@
+// Import Section
+import asyncHandler from "./async.handlers";
+
+// Export Section
+export { asyncHandler };
