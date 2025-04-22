fcb178f4106fb1b7da56ff5b75506be0589a849e
Himanshu Chuchra <chuchrahimanshu@gmail.com>
2025-04-22 11:49:13 +0530

    Create IndividualChat, CommunityChat Schemas

---
+++ src/models/chat/community.chat.models.ts
@@ -0,0 +1,65 @@
+// Import Section
+import mongoose from "mongoose";
+import type { Schema, Model } from "mongoose";
+
+// Schema Section
+const communityChatSchema: Schema<CommunityChatSchemaInterface> =
+  new mongoose.Schema(
+    {
+      developer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Developer",
+        required: true,
+      },
+      content: {
+        type: String,
+        maxlength: 1500,
+        trim: true,
+        required: true,
+      },
+      attachments: [
+        {
+          url: String,
+          id: String,
+        },
+      ],
+      snippets: [
+        {
+          type: String,
+        },
+      ],
+      isEdited: {
+        type: Boolean,
+        required: true,
+        default: false,
+      },
+      referenceLinks: [
+        {
+          title: {
+            type: String,
+            trim: true,
+          },
+          url: {
+            type: String,
+            trim: true,
+          },
+        },
+      ],
+      mentions: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Developer",
+        },
+      ],
+    },
+    {
+      timestamps: true,
+    }
+  );
+
+// Model Section
+const CommunityChat: Model<CommunityChatSchemaInterface> = mongoose.model(
+  "CommunityChat",
+  communityChatSchema
+);
+export default CommunityChat;
---
+++ src/models/chat/individual.chat.models.ts
@@ -0,0 +1,69 @@
+// Import Section
+import mongoose from "mongoose";
+import type { Schema, Model } from "mongoose";
+
+// Schema Section
+const individualChatSchema: Schema<IndividualChatSchemaInterface> =
+  new mongoose.Schema(
+    {
+      sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Developer",
+        required: true,
+      },
+      receiver: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Developer",
+        required: true,
+      },
+      content: {
+        type: String,
+        maxlength: 1500,
+        trim: true,
+        required: true,
+      },
+      attachments: [
+        {
+          url: String,
+          id: String,
+        },
+      ],
+      snippets: [
+        {
+          type: String,
+        },
+      ],
+      isEdited: {
+        type: Boolean,
+        required: true,
+        default: false,
+      },
+      referenceLinks: [
+        {
+          title: {
+            type: String,
+            trim: true,
+          },
+          url: {
+            type: String,
+            trim: true,
+          },
+        },
+      ],
+      status: {
+        type: String,
+        required: true,
+        enum: ["SENT", "DELIVERED", "READ"],
+      },
+    },
+    {
+      timestamps: true,
+    }
+  );
+
+// Model Section
+const IndividualChat: Model<IndividualChatSchemaInterface> = mongoose.model(
+  "IndividualChat",
+  individualChatSchema
+);
+export default IndividualChat;
