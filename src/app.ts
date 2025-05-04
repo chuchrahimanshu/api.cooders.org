8c04676e298434b151b50a3f53634c0d21c2243a
Himanshu Chuchra <chuchrahimanshu@gmail.com>
2025-05-04 21:39:35 +0530

    Integrate CORS for frontend API access

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express from "express";
 import type { Express } from "express";
 import "./config/nodemailer.config";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 // Configuration Section
 const app: Express = express();
@@ -10,6 +11,11 @@ const app: Express = express();
 // Middleware Section
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 // Export Section
 export default app;
