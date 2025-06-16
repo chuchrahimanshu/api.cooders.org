5c58c003bd12633bcf3055527fdb20f69f412de1
Himanshu Chuchra <chuchrahimanshu@gmail.com>
2025-06-16 19:54:35 +0530

    Integrate API Logger using Morgan

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 // Import Section
 import express from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
 
 // Configuration Section
 const app = express();
 
 // Middleware Section
 dotenv.config();
+app.use(morgan("dev"));
 
 // Export Section
 export default app;
